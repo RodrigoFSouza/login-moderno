bcf6288646d6b9276b3750eee174d257fa82ae37
RodrigoFSouza <roferreira122@gmail.com>
2020-09-30 00:35:19 -0300

    criando o projeto

---
+++ functions.js
@@ -0,0 +1,13 @@
+const numeros3 = [3, 8, 9, 15, 12, 29];
+const novoArray = [0],llll
+
+const reordenandoArray = (array, num) => {
+  this.array.forEach(i => {
+    if (num === i) {
+      const numeroEscolhido = num;
+      numeros3.splice(this.numeros3.indexOf(num), 1);
+      console.log(this.numeros);
+      novoArray = [num, ...numeros3];
+    }
+  })
+}
